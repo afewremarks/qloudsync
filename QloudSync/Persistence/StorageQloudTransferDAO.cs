bdd3353029d1984bf655fa03d6edc83b6c369ec7
Angelo Ribeiro <ribeiro.angelo@gmail.com>
2013-03-22 02:41:09 -0300

    Init of persistence layer

---
+++ QloudSync/Persistence/StorageQloudTransferDAO.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using GreenQloud.Repository;
+using System.Linq;
+using System.Xml;
+using System.Threading;
+using GreenQloud.Repository.Model;
+
+namespace GreenQloud.Persistence
+{
+    public class TransferDAOImpl : TransferDAO
+    {
+        #region implemented abstract members of TransferDAO
+
+        public override void Create (TransferResponse transfer)
+        {
+
+        }
+        #endregion
+
+
+    }
+}
+
+
---
+++ QloudSync/Persistence/TransferDAO.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using GreenQloud.Repository;
+using System.Linq;
+using System.Xml;
+using System.Threading;
+using GreenQloud.Repository.Model;
+
+namespace GreenQloud.Persistence
+{
+    public abstract class TransferDAO
+    {
+        public abstract void Create (TransferResponse transfer);
+    }
+}
+
+
