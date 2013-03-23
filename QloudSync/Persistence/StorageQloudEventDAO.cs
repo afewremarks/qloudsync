786c78e241a2d15be92df1fa494270d94a483212
Angelo Ribeiro <ribeiro.angelo@gmail.com>
2013-03-23 00:35:46 -0300

    Create EventDAO

---
+++ QloudSync/Persistence/StorageQloudEventDAO.cs
@@ -0,0 +1,29 @@
+using System;
+using GreenQloud.Repository.Remote;
+using GreenQloud.Repository;
+using GreenQloud.Persistence;
+using GreenQloud.Repository.Local;
+using GreenQloud.Model;
+
+namespace GreenQloud.Persistence
+{
+	public class StorageQloudEventDAO : EventDAO
+	{
+        #region implemented abstract members of EventDAO
+        public override void Create (Event e)
+        {
+            throw new NotImplementedException ();
+        }
+        public override System.Collections.Generic.List<Event> GetEventsNotSynchronized ()
+        {
+            throw new NotImplementedException ();
+        }
+        public override void UpdateToSynchronized (Event e)
+        {
+            throw new NotImplementedException ();
+        }
+        #endregion       
+	}
+
+}
+
