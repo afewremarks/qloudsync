c8113a6b7a44162408ed51ed73cf1cf7cb6d666d
Angelo Ribeiro <ribeiro.angelo@gmail.com>
2013-03-25 08:15:13 -0300

    DAO

---
+++ QloudSync/Persistence/SQLite/SQLiteEventDAO.cs
@@ -0,0 +1,44 @@
+using System;
+using GreenQloud.Repository.Remote;
+using GreenQloud.Repository;
+using GreenQloud.Persistence;
+using GreenQloud.Repository.Local;
+using GreenQloud.Model;
+
+namespace GreenQloud.Persistence
+{
+	public class SQLiteEventDAO : EventDAO
+	{
+        #region implemented abstract members of EventDAO
+
+        public override void Create (Event e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public override System.Collections.Generic.List<Event> All
+        {
+            get{
+                throw new NotImplementedException ();
+            }
+        }
+
+        public override System.Collections.Generic.List<Event> EventsNotSynchronized
+        {
+            get{
+                throw new NotImplementedException ();
+            }
+        }
+
+        public override void UpdateToSynchronized (Event e)
+        {
+            throw new NotImplementedException ();
+        }
+
+        #endregion
+
+
+	}
+
+}
+
---
+++ QloudSync/Persistence/SQLite/SQLiteTransferDAO.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using GreenQloud.Repository;
+using System.Linq;
+using System.Xml;
+using System.Threading;
+using GreenQloud.Model;
+
+namespace GreenQloud.Persistence
+{
+    public class SQLiteTransferDAO : TransferDAO
+    {
+        #region implemented abstract members of TransferDAO
+
+        public override void Create (Transfer transfer)
+        {
+
+        }
+        #endregion
+
+
+    }
+}
+
+
