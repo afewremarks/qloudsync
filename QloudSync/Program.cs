9b4ba4b8997014be5dbcbca0c0ed4e8eb9f3dbba
Angelo Ribeiro <ribeiro.angelo@gmail.com>
2013-04-01 20:42:01 -0300

    Trying fix the connection bug

--- QloudSync/Program.cs
+++ QloudSync/Program.cs
@@ -33,6 +33,7 @@ namespace GreenQloud {
                 UI = new SparkleUI ();
                 UI.Run ();  
             } catch (Exception e){
+                Console.WriteLine (e.Message);
                 Console.WriteLine (e.StackTrace);
                 Logger.LogInfo ("Init", e);
                 Environment.Exit (-1);
