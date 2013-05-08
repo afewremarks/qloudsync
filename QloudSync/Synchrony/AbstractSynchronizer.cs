75bba80aa536f0c59b14ada30252dbbbaf94c6e9
Angelo Ribeiro <ribeiro.angelo@gmail.com>
2013-05-08 01:39:47 -0300

    Moving JSONPost to Util

--- QloudSync/Synchrony/AbstractSynchronizer.cs
+++ QloudSync/Synchrony/AbstractSynchronizer.cs
@@ -125,9 +125,13 @@ namespace GreenQloud.Synchrony
                 transferDAO.Create (transfer);
             logicalLocalRepository.Solve (e.Item);
             eventDAO.UpdateToSynchronized(e);
+<<<<<<< HEAD
             if(e.RepositoryType == RepositoryType.LOCAL){
                 new JSON_POST().send(e);
             }
+=======
+            new JSONPost().send(e);
+>>>>>>> Moving JSONPost to Util
         }
 
 
