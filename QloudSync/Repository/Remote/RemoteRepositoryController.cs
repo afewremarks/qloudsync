9e87754eab479d9dcfea426b1ca1759b29efd186
Gustavo Sizílio <gustavosizilio@gmail.com>
2013-06-02 11:17:25 -0300

    Fix - Adding remote move to transfer type

--- QloudSync/Repository/Remote/RemoteRepositoryController.cs
+++ QloudSync/Repository/Remote/RemoteRepositoryController.cs
@@ -61,6 +61,7 @@ namespace GreenQloud.Repository.Remote
             }
         }
 
+        public abstract Transfer Move (RepositoryItem item);
         public abstract string GetRemoteMD5 (string path);
         public abstract bool ExistsVersion (RepositoryItem file);
         public abstract Transfer Download (RepositoryItem request);
