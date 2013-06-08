910b522c6fdcb193ce592c2642176da70d87e2f7
Gustavo Sizílio <gustavosizilio@gmail.com>
2013-06-08 13:32:24 -0300

    Fix Refactoring RemoteRepositoryController

--- QloudSync/Core/Repository/Local/PhysicalRepositoryController.cs
+++ QloudSync/Core/Repository/Local/PhysicalRepositoryController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace GreenQloud.Repository.Local
 {
-    public abstract class PhysicalRepositoryController : IRepositoryController
+    public abstract class PhysicalRepositoryController
     {
 
         public PhysicalRepositoryController (){
