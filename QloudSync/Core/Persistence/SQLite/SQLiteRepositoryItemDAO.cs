8cf9144d0dc04c91f41aaacdf9501a667d39d52a
Gustavo Sizílio <gustavosizilio@gmail.com>
2013-06-18 23:25:49 -0300

    REFATORING
    ...

--- QloudSync/Core/Persistence/SQLite/SQLiteRepositoryItemDAO.cs
+++ QloudSync/Core/Persistence/SQLite/SQLiteRepositoryItemDAO.cs
@@ -18,7 +18,7 @@ namespace GreenQloud.Persistence.SQLite
             if (item.ResultItem != null)
                 Create (item.ResultItem);
 
-            item.Id = (int) database.ExecuteNonQuery(string.Format("INSERT INTO REPOSITORYITEM (Key, RepositoryId, IsFolder, ResultItemId, eTag, eTagLocal) VALUES (\"{0}\", \"{1}\",\"{2}\",\"{3}\",'{4}','{5}')", item.Key, item.Repository.Id, item.IsFolder, ((item.ResultItem == null) ? "" : item.ResultItem.Id.ToString()), item.ETag, item.LocalETag), true);
+            item.Id = (int) database.ExecuteNonQuery(string.Format("INSERT INTO REPOSITORYITEM (Key, RepositoryId, IsFolder, ResultItemId, eTag, eTagLocal, Moved) VALUES (\"{0}\", \"{1}\",\"{2}\",\"{3}\",'{4}','{5}', '{6}')", item.Key, item.Repository.Id, item.IsFolder, ((item.ResultItem == null) ? "" : item.ResultItem.Id.ToString()), item.ETag, item.LocalETag, item.Moved), true);
         }
         public RepositoryItem Create (Event e)
         {
@@ -39,7 +39,7 @@ namespace GreenQloud.Persistence.SQLite
                 Create (i.ResultItem);
             else if(i.ResultItem != null && i.ResultItem.Id != 0)
                  Update (i.ResultItem);
-            database.ExecuteNonQuery (string.Format("UPDATE REPOSITORYITEM SET  Key='{1}', RepositoryId='{2}', IsFolder='{3}', ResultItemId = '{4}', eTag = '{5}', eTagLocal = '{6}' WHERE RepositoryItemID =\"{0}\"", i.Id, i.Key, i.Repository.Id, i.IsFolder, ((i.ResultItem == null) ? "" : i.ResultItem.Id.ToString()), i.ETag, i.LocalETag));
+            database.ExecuteNonQuery (string.Format("UPDATE REPOSITORYITEM SET  Key='{1}', RepositoryId='{2}', IsFolder='{3}', ResultItemId = '{4}', eTag = '{5}', eTagLocal = '{6}', Moved = '{7}' WHERE RepositoryItemID =\"{0}\"", i.Id, i.Key, i.Repository.Id, i.IsFolder, ((i.ResultItem == null) ? "" : i.ResultItem.Id.ToString()), i.ETag, i.LocalETag, i.Moved));
         }
 
         public override List<RepositoryItem> All {
@@ -65,6 +65,11 @@ namespace GreenQloud.Persistence.SQLite
             return Select(sql).Count != 0 ;
         }
 
+        public bool ExistsUnmoved (RepositoryItem item)
+        {
+            string sql = string.Format("SELECT * FROM REPOSITORYITEM WHERE Key = \"{0}\" AND RepositoryId = \"{1}\" AND Moved = '{2}'", item.Key, item.Repository.Id, bool.FalseString);
+            return Select(sql).Count != 0 ;
+        }
 
         //TODO FIND By KEy e Repo
         public RepositoryItem GetFomDatabase (RepositoryItem item)
@@ -79,6 +84,13 @@ namespace GreenQloud.Persistence.SQLite
         {
             //string sql = string.Format("UPDATE REPOSITORYITEM SET DELETED = \"{0}\" WHERE NAME = \"{1}\" AND RELATIVEPATH = \"{2}\" AND REPOPATH = \"{3}\"", bool.TrueString, item.Name, item.RelativePath, item.Repository.Path);
             //database.ExecuteNonQuery (sql);
+        }
+
+        //TODO DONT NEED
+        public override void MarkAsMoved (RepositoryItem item)
+        {
+            string sql = string.Format("UPDATE REPOSITORYITEM SET Moved = \"{0}\" WHERE RepositoryItemID = \"{1}\" ", bool.TrueString, item.Id);
+            database.ExecuteNonQuery (sql);
         }
 
         public int GetId (RepositoryItem item)
@@ -112,6 +124,7 @@ namespace GreenQloud.Persistence.SQLite
                 item.ResultItem = RepositoryItem.CreateInstance(int.Parse(dr[4].ToString()));
                 item.ETag = dr[5].ToString();
                 item.LocalETag = dr[6].ToString();
+                item.Moved = bool.Parse (dr[7].ToString());
 
                 items.Add (item);
             }
