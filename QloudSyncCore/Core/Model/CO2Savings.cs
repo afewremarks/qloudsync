b22818f715b59192d4c8bfa06f493f9313c735f2
Gustavo Sizílio <gustavosizilio@gmail.com>
2013-07-18 18:22:09 -0300

    Adding CO2 savings

--- QloudSyncCore/Core/Model/CO2Savings.cs
+++ QloudSyncCore/Core/Model/CO2Savings.cs
@@ -9,9 +9,20 @@ namespace GreenQloud.Model
 {
     public class CO2Savings
     {
+        string saved;
         public string Saved {
-            get;
-            set;
+            get {
+                return saved;
+            }
+            set {
+                int v;
+                v = int.Parse(value.Split('.')[0]);
+                if (v <= 1000) {
+                    saved = v.ToString () + "g";
+                } else {
+                    saved = ((float)v/1000).ToString("F1") + "Kg";
+                }
+            }
         }
 
         public string Used {
