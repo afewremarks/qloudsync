4f55ee027690921cbb22ea2281ab60c90c73bd54
Carlos Maia <camilo@camilo.local>
2013-07-19 15:38:40 -0300

    WIP - Adding usage on statistics

---
+++ QloudSyncCore/Core/Model/SQTotalUsed.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Newtonsoft.Json.Linq;
+
+namespace GreenQloud.Model
+{
+    public class SQTotalUsed
+    {
+        string saved;
+        public string Spent {
+            get {
+                return saved;
+            }
+            set {
+                int v;
+                v = int.Parse(value.Split('.')[0]);
+                if (v < (1024^2)) {
+                    saved = ((float)v / 1024).ToString ("F1") + "KB";
+                } else if (v < (1024^3)) {
+                    saved = ((float)v / (1024^2)).ToString ("F1") + "MB";
+                } else {
+                    saved = ((float)v / (1024^3)).ToString ("F1") + "GB";
+                }
+             }
+        }
+
+        public string Used {
+            get;
+            set;
+        }
+    }
+
+}
--- QloudSyncCore/Core/Util/Statistics.cs
+++ QloudSyncCore/Core/Util/Statistics.cs
@@ -29,6 +29,7 @@ namespace GreenQloud
         static void UpdateVariables(){
             versionAvailable = GetVersionAvaliable ();
             savings = GetCO2Statistics ();
+            usedup = GetSQTotalUsed ();
         }
 
         static string versionAvailable = string.Empty;
@@ -66,6 +67,33 @@ namespace GreenQloud
                 }
             }
             return co2savings;
+        }
+
+        static SQTotalUsed usedup = null;
+        public static SQTotalUsed TotalUsedSpace {
+            get {
+                if(usedup==null){
+                    usedup = GetSQTotalUsed();
+                }
+
+                return usedup;
+            }
+        }
+
+        private static SQTotalUsed GetSQTotalUsed (){
+
+            SQTotalUsed sqtotalused = new SQTotalUsed();
+            string hash = Crypto.GetHMACbase64(Credential.SecretKey,Credential.PublicKey, true);
+
+            string uri = string.Format("https://my.greenqloud.com/qloudsync/metrics/?username={0}&hash={1}", Credential.Username, hash);
+            JObject data = JSONHelper.GetInfo (uri);
+            if(data != null && data ["usage"] != null){
+                foreach(JToken o in data ["usage"]){
+                    if((string)o["id"] == "storageqloud_size")
+                        sqtotalused.Spent = (string) o["value"] ;
+                }
+            }
+            return sqtotalused;
         }
 
         private static string GetVersionAvaliable(){
