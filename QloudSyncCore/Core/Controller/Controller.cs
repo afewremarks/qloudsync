cf1228d3b53df04d0d6b0b267cb9b73527350aba
Gustavo Sizílio <gustavosizilio@gmail.com>
2013-08-01 14:40:36 -0300

    Remove unecessary imports

--- QloudSyncCore/Core/Controller/Controller.cs
+++ QloudSyncCore/Core/Controller/Controller.cs
@@ -2,10 +2,6 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
-
-using MonoMac.Foundation;
-using MonoMac.AppKit;
-using MonoMac.ObjCRuntime;
 using GreenQloud.Synchrony;
 using System.Collections.Generic;
 using System.Net.NetworkInformation;
