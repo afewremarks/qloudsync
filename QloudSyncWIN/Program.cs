d86ea93cbdb24ef69371f51c795f391b89fbace0
Gustavo Sizílio <gustavosizilio@gmail.com>
2013-08-02 16:52:51 -0300

    Refactoring project structure

---
+++ QloudSyncWIN/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace QloudSyncWIN
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
