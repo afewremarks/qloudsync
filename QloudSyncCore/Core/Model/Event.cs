032c1a43419c0c945e9eb3e00f76d013b62f9758
Gustavo Sizílio <gustavosizilio@gmail.com>
2013-08-02 13:52:45 -0300

    Fix compile error

--- QloudSyncCore/Core/Model/Event.cs
+++ QloudSyncCore/Core/Model/Event.cs
@@ -126,7 +126,7 @@ using System.Threading;
         {
             CultureInfo cultureInfo   = Thread.CurrentThread.CurrentCulture;
             TextInfo textInfo = cultureInfo.TextInfo;
-            return String.Format ("{0} - {1}", textInfo.ToTitleCase(EventType),  (HaveResultItem ? Item.ResultItem.Key : Item.Key));
+            return String.Format ("{0} - {1}", textInfo.ToTitleCase(EventType.ToString()),  (HaveResultItem ? Item.ResultItem.Key : Item.Key));
         }
 	}
 }
