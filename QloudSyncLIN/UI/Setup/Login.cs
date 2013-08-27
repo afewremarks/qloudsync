11faa1863272f5b23862db2977c523c670a487ec
Gustavo <gustavo@greenqloud.com>
2013-08-27 07:39:50 -0300

    Fix login ui to hidde password

--- QloudSyncLIN/UI/Setup/Login.cs
+++ QloudSyncLIN/UI/Setup/Login.cs
@@ -19,7 +19,7 @@ namespace GreenQloud.UI.Setup
                 Console.WriteLine("Before continue, put your username:");
                 string username = Console.ReadLine();
                 Console.WriteLine("Password:");
-                string password = Console.ReadLine();
+                string password = ReadPassword();
                 try
                 {
                     QloudSync.Repository.S3Connection.Authenticate(username, password);
@@ -31,6 +31,40 @@ namespace GreenQloud.UI.Setup
                     Console.WriteLine("An error ocurred while trying authenticate. Please, try again.");
                 }
             } while (!isConnected);
+        }
+
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
         }
     }
 }
