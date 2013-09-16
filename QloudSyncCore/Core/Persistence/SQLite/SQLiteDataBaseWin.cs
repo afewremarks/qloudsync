c5a229fdd8a9ece9527acf09130c6bd9c8686c6e
Gustavo Sizilio <gustavo@greenqluod.com>
2013-09-16 18:12:42 +0100

    Refactoring SqliteDatabaseWin

--- QloudSyncCore/Core/Persistence/SQLite/SQLiteDataBaseWin.cs
+++ QloudSyncCore/Core/Persistence/SQLite/SQLiteDataBaseWin.cs
@@ -11,13 +11,20 @@ namespace GreenQloud.Persistence.SQLite{
 
     public class SQLiteDatabase
     {
-        String dbConnection;
+
+        private static SQLiteConnection cnn = new SQLiteConnection(ConnectionString);
+        private static SQLiteDatabase instance = new SQLiteDatabase();
 
         public SQLiteDatabase(){
+            cnn.Open();
         }
 
+        public static SQLiteDatabase Instance()
+        {
+            return instance;
+        }
         
-        public string ConnectionString{
+        public static string ConnectionString{
             get{
                 return String.Format("URI=file:{0};Version=3;", RuntimeSettings.DatabaseFile);
             }
@@ -31,22 +38,6 @@ namespace GreenQloud.Persistence.SQLite{
             ExecuteNonQuery (string.Format("INSERT INTO Repository (Path) VALUES (\"{0}\")", RuntimeSettings.HomePath));
         }
 
-
-        /// <summary>
-        ///     Single Param Constructor for specifying advanced connection options.
-        /// </summary>
-        /// <param name="connectionOpts">A dictionary containing all desired options and their values</param>
-        public SQLiteDatabase(Dictionary<String, String> connectionOpts)
-        {
-            String str = "";
-            foreach (KeyValuePair<String, String> row in connectionOpts)
-            {
-                str += String.Format("{0}={1}; ", row.Key, row.Value);
-            }
-            str = str.Trim().Substring(0, str.Length - 1);
-            dbConnection = str;
-        }
-        
         /// <summary>
         ///     Allows the programmer to run a query against the Database.
         /// </summary>
@@ -131,7 +122,6 @@ namespace GreenQloud.Persistence.SQLite{
                     dt.Rows.Add(row);
                 }
                 
-                close(ref reader, ref cnn, ref mycommand);
             }
             catch (Exception e)
             {
@@ -153,21 +143,31 @@ namespace GreenQloud.Persistence.SQLite{
         }
         public int ExecuteNonQuery(string sql, bool returnId)
         {
-            SQLiteConnection cnn = new SQLiteConnection(ConnectionString);
-            cnn.Open();
-            SQLiteCommand mycommand = new SQLiteCommand(cnn);
-            mycommand.CommandText = sql;
-            int result = mycommand.ExecuteNonQuery();
-  
-            if (returnId) {
-                string last_insert_rowid = @"select last_insert_rowid()";
-                mycommand.CommandText = last_insert_rowid; 
-                System.Object temp = mycommand.ExecuteScalar();
-                int id = int.Parse (temp.ToString());
-                return id;
+            SQLiteTransaction tr = cnn.BeginTransaction();
+            int result = 0;
+            try
+            {
+                using (SQLiteCommand mycommand = new SQLiteCommand(cnn))
+                {
+                    mycommand.CommandText = sql;
+                    result = mycommand.ExecuteNonQuery();
+                    tr.Commit();
+                    if (returnId)
+                    {
+                        string last_insert_rowid = @"select last_insert_rowid()";
+                        mycommand.CommandText = last_insert_rowid;
+                        System.Object temp = mycommand.ExecuteScalar();
+                        int id = int.Parse(temp.ToString());
+                        return id;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.LogInfo("ERROR ", e);
+                tr.Rollback();
             }
 
-            close(ref cnn, ref mycommand);
             return result;
         }
         
@@ -178,157 +178,18 @@ namespace GreenQloud.Persistence.SQLite{
         /// <returns>A string.</returns>
         public string ExecuteScalar(string sql)
         {
-            SQLiteConnection cnn = new SQLiteConnection(ConnectionString);
-            cnn.Open();
-            SQLiteCommand mycommand = new SQLiteCommand(cnn);
-            mycommand.CommandText = sql;
-            object value = mycommand.ExecuteScalar();
-            cnn.Close();
-            if (value != null)
+            object value;
+            using (SQLiteCommand mycommand = new SQLiteCommand(cnn))
             {
-                return value.ToString();
-            }
-
-            close(ref cnn, ref mycommand);
-
-            return "";
-        }
-        
-        /// <summary>
-        ///     Allows the programmer to easily update rows in the DB.
-        /// </summary>
-        /// <param name="tableName">The table to update.</param>
-        /// <param name="data">A dictionary containing Column names and their new values.</param>
-        /// <param name="where">The where clause for the update statement.</param>
-        /// <returns>A boolean true or false to signify success or failure.</returns>
-        public bool Update(String tableName, Dictionary<String, String> data, String where)
-        {
-            String vals = "";
-            Boolean returnCode = true;
-            if (data.Count >= 1)
-            {
-                foreach (KeyValuePair<String, String> val in data)
+                mycommand.CommandText = sql;
+                value = mycommand.ExecuteScalar();
+                if (value != null)
                 {
-                    vals += String.Format(" {0} = '{1}',", val.Key.ToString(), val.Value.ToString());
+                    return value.ToString();
                 }
-                vals = vals.Substring(0, vals.Length - 1);
+                return "";
             }
-            try
-            {
-                this.ExecuteNonQuery(String.Format("update {0} set {1} where {2};", tableName, vals, where));
-            }
-            catch
-            {
-                returnCode = false;
-            }
-            return returnCode;
-        }
-        
-        /// <summary>
-        ///     Allows the programmer to easily delete rows from the DB.
-        /// </summary>
-        /// <param name="tableName">The table from which to delete.</param>
-        /// <param name="where">The where clause for the delete.</param>
-        /// <returns>A boolean true or false to signify success or failure.</returns>
-        public bool Delete(String tableName, String where)
-        {
-            Boolean returnCode = true;
-            try
-            {
-                this.ExecuteNonQuery(String.Format("delete from {0} where {1};", tableName, where));
-            }
-            catch (Exception fail)
-            {
-                Logger.LogInfo("ERROR", fail.Message);
-                returnCode = false;
-            }
-            return returnCode;
-        }
-        
-        /// <summary>
-        ///     Allows the programmer to easily insert into the DB
-        /// </summary>
-        /// <param name="tableName">The table into which we insert the data.</param>
-        /// <param name="data">A dictionary containing the column names and data for the insert.</param>
-        /// <returns>A boolean true or false to signify success or failure.</returns>
-        public bool Insert(String tableName, Dictionary<String, String> data)
-        {
-            String columns = "";
-            String values = "";
-            Boolean returnCode = true;
-            foreach (KeyValuePair<String, String> val in data)
-            {
-                columns += String.Format(" {0},", val.Key.ToString());
-                values += String.Format(" '{0}',", val.Value);
-            }
-            columns = columns.Substring(0, columns.Length - 1);
-            values = values.Substring(0, values.Length - 1);
-            try
-            {
-                this.ExecuteNonQuery(String.Format("insert into {0}({1}) values({2});", tableName, columns, values));
-            }
-            catch(Exception fail)
-            {
-                returnCode = false;
-            }
-            return returnCode;
-        }
-        
-        /// <summary>
-        ///     Allows the programmer to easily delete all data from the DB.
-        /// </summary>
-        /// <returns>A boolean true or false to signify success or failure.</returns>
-        public bool ClearDB()
-        {
-            DataTable tables;
-            try
-            {
-                tables = this.GetDataTable("select NAME from SQLITE_MASTER where type='table' order by NAME;");
-                foreach (DataRow table in tables.Rows)
-                {
-                    this.ClearTable(table["NAME"].ToString());
-                }
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-        
-        /// <summary>
-        ///     Allows the user to easily clear all data from a specific table.
-        /// </summary>
-        /// <param name="table">The name of the table to clear.</param>
-        /// <returns>A boolean true or false to signify success or failure.</returns>
-        public bool ClearTable(String table)
-        {
-            try
-            {
-                
-                this.ExecuteNonQuery(String.Format("delete from {0};", table));
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }       
-
-        private void close (ref SQLiteConnection cnn, ref SQLiteCommand mycommand)
-        {
-            mycommand.Dispose ();
-            mycommand = null;
-            cnn.Close ();
-            cnn = null;
-        }
-
-        private void close (ref SQLiteDataReader reader, ref SQLiteConnection cnn, ref SQLiteCommand mycommand)
-        {
-            reader.Close ();
-            reader = null;
-            close(ref cnn, ref mycommand);
-        }
+        }               
     }
 }
 #endif
