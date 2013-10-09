676964be81be78be4f729cfde7e3d9693ba97f1f
Carlos Maia <cwmaia@gmail.com>
2013-10-09 16:19:59 -0300

    changed network manager window

--- QloudSyncWIN/UI/NetworkManager.Designer.cs
+++ QloudSyncWIN/UI/NetworkManager.Designer.cs
@@ -28,20 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NetworkManager));
             this.totalBandwidthConsumptionLabel = new System.Windows.Forms.Label();
             this.currentBandwidthConsumptionLabel = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.numberofitems = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // totalBandwidthConsumptionLabel
             // 
             this.totalBandwidthConsumptionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.totalBandwidthConsumptionLabel.AutoSize = true;
+            this.totalBandwidthConsumptionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.totalBandwidthConsumptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalBandwidthConsumptionLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.totalBandwidthConsumptionLabel.Location = new System.Drawing.Point(12, 9);
             this.totalBandwidthConsumptionLabel.Name = "totalBandwidthConsumptionLabel";
-            this.totalBandwidthConsumptionLabel.Size = new System.Drawing.Size(151, 13);
+            this.totalBandwidthConsumptionLabel.Size = new System.Drawing.Size(179, 13);
             this.totalBandwidthConsumptionLabel.TabIndex = 0;
             this.totalBandwidthConsumptionLabel.Text = "Total Bandwidth Consumption:";
             // 
@@ -49,50 +52,49 @@
             // 
             this.currentBandwidthConsumptionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.currentBandwidthConsumptionLabel.AutoSize = true;
+            this.currentBandwidthConsumptionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.currentBandwidthConsumptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentBandwidthConsumptionLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.currentBandwidthConsumptionLabel.Location = new System.Drawing.Point(12, 31);
             this.currentBandwidthConsumptionLabel.Name = "currentBandwidthConsumptionLabel";
-            this.currentBandwidthConsumptionLabel.Size = new System.Drawing.Size(161, 13);
+            this.currentBandwidthConsumptionLabel.Size = new System.Drawing.Size(191, 13);
             this.currentBandwidthConsumptionLabel.TabIndex = 1;
             this.currentBandwidthConsumptionLabel.Text = "Current Bandwidth Consumption:";
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(15, 57);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(576, 161);
-            this.panel1.TabIndex = 2;
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(473, 25);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(100, 23);
-            this.progressBar1.TabIndex = 0;
             // 
             // numberofitems
             // 
             this.numberofitems.AutoSize = true;
-            this.numberofitems.Location = new System.Drawing.Point(470, 9);
+            this.numberofitems.BackColor = System.Drawing.Color.Transparent;
+            this.numberofitems.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberofitems.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.numberofitems.Location = new System.Drawing.Point(502, 9);
             this.numberofitems.Name = "numberofitems";
-            this.numberofitems.Size = new System.Drawing.Size(87, 13);
+            this.numberofitems.Size = new System.Drawing.Size(104, 13);
             this.numberofitems.TabIndex = 3;
             this.numberofitems.Text = "Items in Process:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 59);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(593, 23);
+            this.progressBar1.TabIndex = 4;
             // 
             // NetworkManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(603, 230);
+            this.BackgroundImage = global::GreenQloud.Properties.Resources.Network;
+            this.ClientSize = new System.Drawing.Size(624, 266);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.numberofitems);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.currentBandwidthConsumptionLabel);
             this.Controls.Add(this.totalBandwidthConsumptionLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "NetworkManager";
-            this.ShowIcon = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Network Status";
@@ -105,8 +107,7 @@
 
         private System.Windows.Forms.Label totalBandwidthConsumptionLabel;
         private System.Windows.Forms.Label currentBandwidthConsumptionLabel;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label numberofitems;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- QloudSyncWIN/UI/Setup/ConfFolders.Designer.cs
+++ QloudSyncWIN/UI/Setup/ConfFolders.Designer.cs
@@ -28,6 +28,7 @@ namespace GreenQloud.UI.Setup
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfFolders));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -89,6 +90,7 @@ namespace GreenQloud.UI.Setup
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "ConfFolders";
             this.ShowIcon = false;
