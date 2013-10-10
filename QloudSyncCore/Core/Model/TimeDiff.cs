5081de10876b622df584e4207693a641968fe812
gustavosizilio <gustavosizilio@gmail.com>
2013-10-10 15:25:31 -0300

    Refactoring database to ravendb and other bug stufs :P

---
+++ QloudSyncCore/Core/Model/TimeDiff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GreenQloud.Model
+{
+    class TimeDiff
+    {
+        public int Id { get; set; }
+        public double Diff {get; set;}
+    }
+}
