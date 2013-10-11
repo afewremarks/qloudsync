e431386dee0a09de33090940687396c7aab736e9
Carlos Maia <cwmaia@gmail.com>
2013-10-11 15:03:17 -0300

    Preparation for the progress bar implementation, upload now showing

--- QloudSyncWIN/UI/NetworkManager.cs
+++ QloudSyncWIN/UI/NetworkManager.cs
@@ -1,6 +1,4 @@
-﻿using GreenQloud.Model;
-using QloudSyncCore.Core.Persistence;
-using QloudSyncCore.Core.Util;
+﻿using QloudSyncCore.Core.Util;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,6 +19,7 @@ namespace GreenQloud.UI
         NetworkTraffic trafficMonitor = null;
         Timer bandwidthCalcTimer = new Timer();
         float lastAmountOfBytesReceived;
+        float lastAmountOfBytesSent;
 
         
 
@@ -36,12 +35,12 @@ namespace GreenQloud.UI
 
         void bandwidthCalcTimer_Tick(object sender, EventArgs e)
         {
-            EventRaven eventDao = new EventRaven();
-            List<Event> events = eventDao.LastEvents;
 
             float currentAmountOfBytesReceived = trafficMonitor.GetBytesReceived();
+            float currentAmountofBytesSent = trafficMonitor.GetBytesSent();
             totalBandwidthConsumptionLabel.Text = string.Format("Total Bandwidth Consumption: {0} kb", (currentAmountOfBytesReceived / 1024).ToString("0.00"));
-            currentBandwidthConsumptionLabel.Text = string.Format("Current Bandwidth Consumption: {0} kb/sec", (((currentAmountOfBytesReceived - lastAmountOfBytesReceived) / 1024)).ToString("0.00"));
+            currentBandwidthDownloadLabel.Text = string.Format("Current Download Bandwidth: {0} kb/sec", (((currentAmountOfBytesReceived - lastAmountOfBytesReceived) / 1024)).ToString("0.00"));
+            currentBandwidthUploadLabel.Text = string.Format("Current Upload Bandwidth: {0} kb/sec", Math.Abs(((currentAmountofBytesSent - lastAmountOfBytesSent) / 1024)).ToString("0.00"));
             if (((currentAmountOfBytesReceived - lastAmountOfBytesReceived) / 1024) > 1)
             {
                 numberofitems.Text = string.Format("Items in Process: {0}", 1);
@@ -50,8 +49,8 @@ namespace GreenQloud.UI
             {
                 numberofitems.Text = string.Format("Items in Process: {0}", 0);
             }
-            //Progress Bar
             lastAmountOfBytesReceived = currentAmountOfBytesReceived;
+            lastAmountOfBytesSent = currentAmountofBytesSent;
         }
 
         private void downloadSampleFileButton_Click_1(object sender, EventArgs e)
@@ -61,5 +60,11 @@ namespace GreenQloud.UI
 
             client.DownloadFileAsync(new Uri(url), Path.GetTempFileName());
         }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
