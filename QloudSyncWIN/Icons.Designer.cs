471da03ee8c59b312999b5498071152839bed301
Carlos Maia <cwmaia@gmail.com>
2013-10-17 16:37:23 -0300

    fixes to several bugs

--- QloudSyncWIN/Icons.Designer.cs
+++ QloudSyncWIN/Icons.Designer.cs
@@ -239,5 +239,15 @@ namespace GreenQloud {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        public static System.Drawing.Icon qloudsync_ico {
+            get {
+                object obj = ResourceManager.GetObject("qloudsync_ico", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
