4dd7508bdeab950dbcfa1427d47efb100bd272ab
gustavosizilio <gustavosizilio@gmail.com>
2013-10-31 13:50:51 -0300

    manual bug report

--- QloudSyncWIN/UI/BugReport.Designer.cs
+++ QloudSyncWIN/UI/BugReport.Designer.cs
@@ -111,6 +111,7 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "Submit Report";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
